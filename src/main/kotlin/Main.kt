33c66c87a522d06fdc1887cfc5a9be91fd64839c
Cocodilla <alexakitten@yandex.com>
2025-06-14 01:00:43 +0300

    11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,38 @@
 package ru.netology
+fun main(){
+
+}
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how GIGA IDE suggests fixing it.
-    println("Hello, " + name + "!")
+fun calculateCommission(
+    cardType: String = "VK Pay",
+    previousTransfers: Int = 0,
+    transferAmount: Int
+): Int {
+    // Проверка лимитов для VK Pay
+    if (cardType == "VK Pay") {
+        if (transferAmount > 15_000) return -1
+        if (previousTransfers + transferAmount > 40_000) return -1
+        return 0
+    }
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    // Проверка лимитов для банковских карт
+    if (transferAmount > 150_000) return -1
+    if (previousTransfers + transferAmount > 600_000) return -1
+
+    return when (cardType) {
+        "Mastercard", "Maestro" -> {
+            if (transferAmount >= 300 && previousTransfers + transferAmount <= 75_000) {
+                0
+            } else {
+                (transferAmount * 0.006).toInt() + 20
+            }
+        }
+        "Visa", "Мир" -> {
+            val commission = (transferAmount * 0.0075).toInt()
+            if (commission < 35) 35 else commission
+        }
+        else -> 0
     }
 }
